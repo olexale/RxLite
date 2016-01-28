4c372830aaf42e9d9d395cd19709d48a494c1fc5
Olexandr Leuschenko <ole@ciklum.com>
2016-01-28 23:06:14 +0200

    Playground.Droid added

---
+++ Playground/Playground.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Playground.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace Playground.Droid
+{
+    [Activity(Label = "Playground.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
